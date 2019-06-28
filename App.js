bcb3b535734c2e98295c154ea7b7aedcd2f4c6b1
emessell <emessell22@gmail.com>
2019-06-28 14:13:19 +0900

    20190528.14:13
    zz

--- App.js
+++ App.js
@@ -5,33 +5,22 @@ const Prompt =  window.ReactRouterDOM.Prompt;
 const Switch = window.ReactRouterDOM.Switch;
 const Redirect = window.ReactRouterDOM.Redirect;
 
-class Welcome extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello, {this.props.name}</h1>
-        <div id="chart_div"></div>
-      </div>
-    );
-  }
-}
-
 class App extends React.Component {
   render(){
     return (
       <div>
-        <BasicExample></BasicExample>
+        <Navigation></Navigation>
       </div>
     )
   }
 }
 
-class BasicExample extends Router {
+class Navigation extends Router {
   render(){
     return (
       <Router>
         <div>
-          <ul>
+          <ul className='navBar'>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/topics">Topics</Link></li>
@@ -72,9 +61,11 @@ function Home() {
   }
 
   return (
-    <div>
+    <div className='content'>
       <h2>Home</h2>
-      <Welcome name='Canma'></Welcome>
+      <Clock/>
+      <Calculator/>
+      <div id="chart_div"></div>
     </div>
   );
 }
@@ -109,9 +100,9 @@ function About() {
     };
     var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
     chart.draw(view, options);
-}
+  }
   return (
-    <div>
+    <div className='content'>
       <h2>About</h2>
       <p>두번째 페이지입니다.</p>
       <div id="columnchart_values"></div>
@@ -121,7 +112,7 @@ function About() {
 
 function Topics({ match }) {
   return (
-    <div>
+    <nav className='content'>
       <h2>Topics</h2>
       <ul>
         <li>
@@ -141,7 +132,7 @@ function Topics({ match }) {
         path={match.path}
         render={() => <h3>Please select a topic.</h3>}
       />
-    </div>
+    </nav>
   );
 }
 
@@ -151,6 +142,73 @@ function Topic({ match }) {
       <h3>{match.params.topicId}</h3>
     </div>
   );
+}
+
+class Clock extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {date : new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        {this.state.date.toLocaleTimeString()}
+      </div>
+    )
+  }
+}
+
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {temperature: ''};
+  }
+
+  handleChange(e) {
+    this.setState({temperature: e.target.value});
+  }
+
+  render() {
+    const temperature = this.state.temperature;
+    return (
+      <fieldset>
+        <legend>Enter temperature in Celsius:</legend>
+        <input
+          value={temperature}
+          onChange={this.handleChange} />
+
+        <BoilingVerdict
+          celsius={parseFloat(temperature)} />
+
+      </fieldset>
+    );
+  }
 }
 
 ReactDOM.render(
